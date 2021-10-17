f3a292987f84e869b54a52316794e9be14679017
Yaser Andrade <77845161+Yaser-AndOr@users.noreply.github.com>
2021-10-17 07:05:54 -0600

    Implementación de benchmarks

---
+++ Menu/Menu/Benchmark.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Menu
+{
+    partial class Benchmark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Benchmark));
+            this.CerrarBtn = new System.Windows.Forms.PictureBox();
+            this.SubemuBtn = new System.Windows.Forms.PictureBox();
+            this.TempLbl = new System.Windows.Forms.Label();
+            this.CpuLbl = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Menu = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.EjecutarBtn = new System.Windows.Forms.PictureBox();
+            this.ResultadoLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CerrarBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SubemuBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EjecutarBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CerrarBtn
+            // 
+            this.CerrarBtn.BackColor = System.Drawing.Color.Transparent;
+            this.CerrarBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CerrarBtn.Image = ((System.Drawing.Image)(resources.GetObject("CerrarBtn.Image")));
+            this.CerrarBtn.Location = new System.Drawing.Point(741, 2);
+            this.CerrarBtn.Name = "CerrarBtn";
+            this.CerrarBtn.Size = new System.Drawing.Size(100, 50);
+            this.CerrarBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CerrarBtn.TabIndex = 0;
+            this.CerrarBtn.TabStop = false;
+            this.CerrarBtn.Click += new System.EventHandler(this.CerrarBtn_Click);
+            // 
+            // SubemuBtn
+            // 
+            this.SubemuBtn.BackColor = System.Drawing.Color.Transparent;
+            this.SubemuBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SubemuBtn.Image = ((System.Drawing.Image)(resources.GetObject("SubemuBtn.Image")));
+            this.SubemuBtn.Location = new System.Drawing.Point(742, 74);
+            this.SubemuBtn.Name = "SubemuBtn";
+            this.SubemuBtn.Size = new System.Drawing.Size(100, 50);
+            this.SubemuBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SubemuBtn.TabIndex = 1;
+            this.SubemuBtn.TabStop = false;
+            this.SubemuBtn.Click += new System.EventHandler(this.Submenu_Click);
+            // 
+            // TempLbl
+            // 
+            this.TempLbl.AutoSize = true;
+            this.TempLbl.BackColor = System.Drawing.Color.Transparent;
+            this.TempLbl.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TempLbl.Location = new System.Drawing.Point(29, 114);
+            this.TempLbl.Name = "TempLbl";
+            this.TempLbl.Size = new System.Drawing.Size(0, 75);
+            this.TempLbl.TabIndex = 2;
+            // 
+            // CpuLbl
+            // 
+            this.CpuLbl.AutoSize = true;
+            this.CpuLbl.BackColor = System.Drawing.Color.Transparent;
+            this.CpuLbl.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CpuLbl.Location = new System.Drawing.Point(29, 297);
+            this.CpuLbl.Name = "CpuLbl";
+            this.CpuLbl.Size = new System.Drawing.Size(0, 75);
+            this.CpuLbl.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(637, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.Menu.Location = new System.Drawing.Point(637, 130);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(205, 23);
+            this.Menu.TabIndex = 5;
+            this.Menu.Text = "Menu";
+            this.Menu.UseVisualStyleBackColor = true;
+            this.Menu.Click += new System.EventHandler(this.Menu_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(637, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(205, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // EjecutarBtn
+            // 
+            this.EjecutarBtn.BackColor = System.Drawing.Color.Transparent;
+            this.EjecutarBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EjecutarBtn.Image = ((System.Drawing.Image)(resources.GetObject("EjecutarBtn.Image")));
+            this.EjecutarBtn.Location = new System.Drawing.Point(278, 158);
+            this.EjecutarBtn.Name = "EjecutarBtn";
+            this.EjecutarBtn.Size = new System.Drawing.Size(247, 116);
+            this.EjecutarBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.EjecutarBtn.TabIndex = 8;
+            this.EjecutarBtn.TabStop = false;
+            this.EjecutarBtn.Click += new System.EventHandler(this.EjecutarBtn_Click);
+            // 
+            // ResultadoLbl
+            // 
+            this.ResultadoLbl.AutoSize = true;
+            this.ResultadoLbl.BackColor = System.Drawing.Color.Transparent;
+            this.ResultadoLbl.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ResultadoLbl.Location = new System.Drawing.Point(232, 297);
+            this.ResultadoLbl.Name = "ResultadoLbl";
+            this.ResultadoLbl.Size = new System.Drawing.Size(0, 29);
+            this.ResultadoLbl.TabIndex = 9;
+            // 
+            // Benchmark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(841, 426);
+            this.Controls.Add(this.ResultadoLbl);
+            this.Controls.Add(this.EjecutarBtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.CpuLbl);
+            this.Controls.Add(this.TempLbl);
+            this.Controls.Add(this.SubemuBtn);
+            this.Controls.Add(this.CerrarBtn);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Benchmark";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Benchmark";
+            ((System.ComponentModel.ISupportInitialize)(this.CerrarBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SubemuBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EjecutarBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CerrarBtn;
+        private System.Windows.Forms.PictureBox SubemuBtn;
+        private System.Windows.Forms.Label TempLbl;
+        private System.Windows.Forms.Label CpuLbl;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Menu;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox EjecutarBtn;
+        private System.Windows.Forms.Label ResultadoLbl;
+    }
+}
