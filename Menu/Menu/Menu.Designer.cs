408dbebc02da36806809a2945dca0ed156ede7f4
Yaser Andrade <77845161+Yaser-AndOr@users.noreply.github.com>
2021-10-17 04:22:19 -0600

    Menu Principal
    Creación del menú principal del programa

---
+++ Menu/Menu/Menu.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Menu
+{
+    partial class Menu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SalirBtn = new System.Windows.Forms.PictureBox();
+            this.TempUs = new System.Windows.Forms.Label();
+            this.CPUUs = new System.Windows.Forms.Label();
+            this.LimpiarBtn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SalirBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LimpiarBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(627, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(732, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.Submenu_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(627, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(627, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(205, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // SalirBtn
+            // 
+            this.SalirBtn.BackColor = System.Drawing.Color.Transparent;
+            this.SalirBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SalirBtn.Image = ((System.Drawing.Image)(resources.GetObject("SalirBtn.Image")));
+            this.SalirBtn.Location = new System.Drawing.Point(741, 2);
+            this.SalirBtn.Name = "SalirBtn";
+            this.SalirBtn.Size = new System.Drawing.Size(100, 50);
+            this.SalirBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SalirBtn.TabIndex = 6;
+            this.SalirBtn.TabStop = false;
+            this.SalirBtn.Click += new System.EventHandler(this.SalirBtn_Click);
+            // 
+            // TempUs
+            // 
+            this.TempUs.AutoSize = true;
+            this.TempUs.BackColor = System.Drawing.Color.Transparent;
+            this.TempUs.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TempUs.Location = new System.Drawing.Point(29, 114);
+            this.TempUs.Name = "TempUs";
+            this.TempUs.Size = new System.Drawing.Size(0, 75);
+            this.TempUs.TabIndex = 7;
+            // 
+            // CPUUs
+            // 
+            this.CPUUs.AutoSize = true;
+            this.CPUUs.BackColor = System.Drawing.Color.Transparent;
+            this.CPUUs.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CPUUs.Location = new System.Drawing.Point(29, 297);
+            this.CPUUs.Name = "CPUUs";
+            this.CPUUs.Size = new System.Drawing.Size(0, 75);
+            this.CPUUs.TabIndex = 8;
+            // 
+            // LimpiarBtn
+            // 
+            this.LimpiarBtn.BackColor = System.Drawing.Color.Transparent;
+            this.LimpiarBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LimpiarBtn.Image = ((System.Drawing.Image)(resources.GetObject("LimpiarBtn.Image")));
+            this.LimpiarBtn.Location = new System.Drawing.Point(278, 158);
+            this.LimpiarBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.LimpiarBtn.Name = "LimpiarBtn";
+            this.LimpiarBtn.Size = new System.Drawing.Size(247, 116);
+            this.LimpiarBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LimpiarBtn.TabIndex = 9;
+            this.LimpiarBtn.TabStop = false;
+            this.LimpiarBtn.Click += new System.EventHandler(this.LimpiarBtn_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(841, 426);
+            this.Controls.Add(this.LimpiarBtn);
+            this.Controls.Add(this.CPUUs);
+            this.Controls.Add(this.TempUs);
+            this.Controls.Add(this.SalirBtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SalirBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LimpiarBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox SalirBtn;
+        private System.Windows.Forms.Label TempUs;
+        private System.Windows.Forms.Label CPUUs;
+        private System.Windows.Forms.PictureBox LimpiarBtn;
+    }
+}
+
---
+++ Menu/Menu/Menu.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+using System.IO;
+
+namespace Menu
+{
+    public partial class Menu : Form
+    {
+        private static PerformanceCounter pcPerf = new PerformanceCounter("Thermal zone information", "Temperature", @"\_TZ.THRM");
+        private static PerformanceCounter uso = new PerformanceCounter("Processor information", "% Processor time", @"_Total");
+        public void medidor()
+        {
+            TempUs.Text = "" + ((int)pcPerf.NextValue() - 273);
+            CPUUs.Text = "" + ((int)uso.NextValue()) + "%";
+        }
+
+        public Menu()
+        {
+            InitializeComponent();
+            Carga_Menu();
+        }
+        private void Carga_Menu()
+        {
+            Timer Temporizador = new Timer();
+            Temporizador.Interval = 25;
+            Temporizador.Tick += new EventHandler(Actualizador);
+            Temporizador.Start();
+            button1.Hide();
+            button3.Hide();
+            button4.Hide();
+        }
+        private int pr = 0, anim, i = 0, px = 0, py = 0;
+
+        private void SalirBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Submenu_Click(object sender, EventArgs e)
+        {
+            if (anim == 0)
+            {
+                py = pictureBox1.Location.Y + pictureBox1.Height;
+                anim = 1;
+                switch (pr)
+                {
+                    case 0:
+                        px = this.Width + 1;
+                        button1.Location = new Point(px, py + 2);
+                        button3.Location = new Point(px, py + 30);
+                        button4.Location = new Point(px, py + 58);
+                        button1.Show();
+                        button3.Show();
+                        button4.Show();
+                        i = px;
+                        System.Threading.Thread.Sleep(10);
+                        animacionaparecer();
+                        break;
+                    case 1:
+                        px = this.Width - 200;
+                        button1.Location = new Point(px, py + 2);
+                        button3.Location = new Point(px, py + 30);
+                        button4.Location = new Point(px, py + 58);
+                        i = px;
+                        animaciondesaparecer();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        private static int Archborrados = 0, CarpBorradas = 0;
+        private static void BorrarTemporales(string ruta, bool borrar)
+        {
+            List<string> listaArchivos = Directory.GetFiles(ruta).ToList();
+            foreach (string archivo in listaArchivos)
+            {
+                try
+                {
+                    File.Delete(archivo);
+                    Archborrados++;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            List<string> lstChilFolder = Directory.GetDirectories(ruta).ToList();
+            foreach (string pathFolder in lstChilFolder)
+            {
+                try
+                {
+                    BorrarTemporales(pathFolder, true);
+                    CarpBorradas++;
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (borrar)
+            {
+                Directory.Delete(ruta);
+            }
+        }
+        private void LimpiarBtn_Click(object sender, EventArgs e)
+        {
+            String lugar = $"c:\\Users\\{Environment.UserName}\\AppData\\Local\\temp";
+            BorrarTemporales(lugar, false);
+            lugar = $"C:\\Windows\\temp";
+            BorrarTemporales(lugar, false);
+            MessageBox.Show($"Se han borrado {Archborrados} archivos, y {CarpBorradas} carpetas exitosamente.");
+            Archborrados = 0; CarpBorradas = 0;
+        }
+        private void animacionaparecer()
+        {
+            i -= 10;
+            button1.Location = new Point(i, py + 2);
+            button3.Location = new Point(i, py + 30);
+            button4.Location = new Point(i, py + 58);
+            if (i < this.Width - 200)
+            {
+                anim = 0;
+                pr = 1;
+            }
+        }
+        private void animaciondesaparecer()
+        {
+            i += 10;
+            button1.Location = new Point(i, py + 2);
+            button3.Location = new Point(i, py + 30);
+            button4.Location = new Point(i, py + 58);
+            if (i > this.Width + 1)
+            {
+                button1.Hide();
+                button3.Hide();
+                button4.Hide();
+                anim = 0;
+                pr = 0;
+            }
+        }
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+        private int cuantasveces = 0;
+        private void Actualizador(object remitente, EventArgs e)
+        {
+            cuantasveces++;
+            if (cuantasveces >= 20)
+            {
+                cuantasveces = 0;
+                medidor();
+            }
+            if (anim == 1)
+            {
+                switch (pr)
+                {
+                    case 0:
+                        animacionaparecer();
+                        break;
+                    case 1:
+                        animaciondesaparecer();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
